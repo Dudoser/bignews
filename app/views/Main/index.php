9926df2e84e016d2581c22e118c16893e4c1f2fa
WesT <anton.white.lion@gmail.com>
2018-07-05 14:34:17 +0300

    filter

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -1,18 +1,44 @@
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="../../../public/images/1.jpeg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="../../../public/images/2.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="../../../public/images/3.jpg" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>  
+
+
 <span id="result"></span>
 <br>
 <br>
 <br>
 <br>
+
+
 <?php for ($i=0; $i < count($news)+1; $i++) :?>
         <div class="col-md-6 col-lg-6 col-sm-6 col-xl-6 vi">
             <div class="panel-body">
                 <a href="/category/view/?id=<?=$news[$i][0]['category_id']?>&page=1"><?= $news[$i][0]['category'] ?></a>
+
             </div>
             <?php foreach ($news[$i] as $article): ?>
                 <div class="col-md-12 col-lg-12 col-sm-12 col-xl-12 ">
-                    <div class="panel-heading"><a href="#"><?= $article['title'] ?></a></div>
+                    <div class="panel-heading"><a href="/news/index/?id=<?=$article['id'];?>"><?= $article['title'] ?></a></div>
                     <div class="panel-body">
-                        <?= $post['name'] ?>
+                        <?= $post['name']; ?>
                     </div>
                     <div class="panel-body">
                         <?php if (is_array($article['tag'])): ?>
@@ -31,4 +57,5 @@
                 </div>
             <?php endforeach; ?>
         </div>
+        <img src="../../../public/images/1.jpeg">
 <?php endfor;?>
--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -28,21 +28,127 @@
                     </div>
                     <select multiple class="form-control" id="list-search-form">
                     </select>
+
                 </div>
+            </div>
+
+
+<div class="btn-group">
+  <button type="button" class="btn btn-danger">Action</button>
+  <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <span class="sr-only">Toggle Dropdown</span>
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="#">Separated link</a>
+  </div>
+</div>
+
+            <div>
+                <form method="POST" action="/Main/search/">
+                    <select name="sel_1">
+
+                        <option value="any">Any</option>
+
+                        <?php foreach ($categories as $category): ?>
+                            <option><?php echo $category['name']; ?></option>
+                        <?php endforeach; ?>
+
+                    </select>
+
+                    <select name="sel_2">
+                        <option value="any">Any</option>
+
+                        <?php foreach ($categories as $category): ?>
+                            <option><?php echo $category['name']; ?></option>
+                        <?php endforeach; ?>  
+
+                    </select>
+
+                    <input type="date" name="calendar_1">
+                    <input type="date" name="calendar_2"> 
+                    <input type="submit" name="submit_filter" value="filter">
+                </form>
             </div>
         </header>
         
         <marquee>
-            <h1>Ах ты ж ёб твою мать!</h1>
+            <h1>Курс валют и погода</h1>
         </marquee>
-        <div class="container">
-            <?=$content?>
+
+      
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-2">
+                    
+                </div>
+                <div class="col-8">
+                    <?=$content?>
+                </div>
+                <div class="col-2">
+              
+                   <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img class="d-block w-100" src="../../../public/images/3.jpg" alt="First slide">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h3>Битконы</h3>
+                            <p>ваучеры</p>
+                          </div>
+                        </div>
+                        <div class="carousel-item">
+                          <img class="d-block w-100" src="../../../public/images/2.jpg" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                          <img class="d-block w-100" src="../../../public/images/3.jpg" alt="Third slide">
+                        </div>
+                      </div>
+                    </div>
+    
+                </div>
+            
+            </div>
         </div>
+
         <?php debug(vendor\core\Db::$countSql)?>
         <?php debug(vendor\core\Db::$queries)?>
+
+         <div class="modal fade">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title">BigNews.com</h4>
+              </div>
+              <div class="modal-body" align="center">
+                <p>Подпишитесь на новости портала.</p>
+                <input type="email" name="nameFollow" placeholder="Ваш email...">
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Send</button>
+              </div>
+            </div>
+          </div>
+        </div> 
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
         <script src="../../../public/js/search.js"></script>
+        <script type="text/javascript">
+            setTimeout(function mod() {
+
+                //$('.modal').modal("show");
+            }, 5000);
+
+        </script>
+
+
     </body>
 </html>
